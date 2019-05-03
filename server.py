58376bf0d7bdd2eb0c2620f3a702a4325866e5b1
Sicilat <sicilat@gmail.com>
2019-05-03 19:23:36 +0200

    Support database + Login function

--- server.py
+++ server.py
@@ -1,22 +1,39 @@
-import socket, threading, datetime, time, random, sqlite3
+import socket, threading, datetime, time, sqlite3, pickle
+
+def create_tables():
+    db_connection = sqlite3.connect('database.db')
+    cursor = db_connection.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS users(datestamp TEXT, email TEXT, password TEXT)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS storage(datestamp TEXT, email TEXT, password TEXT, about TEXT)')
+    cursor.close()
+    db_connection.close()
 
 def get_time():
     return str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
 
+def log_in(self, data):
+    db_connection = sqlite3.connect('database.db')
+    cursor = db_connection.cursor()
+    sql = "SELECT * FROM users WHERE email='" + str(data[1]) + "' AND password='" + str(data[2]) + "'"
+    cursor.execute(sql)
+    result = len(cursor.fetchall())
+    if result == 1:
+        self.csocket.send(bytes('log_fine','UTF-8'))
+        print(get_time() + " - Client at " +  str(clientAddress) + " logged in with email : " + str(data[1]))
+    else:
+        self.csocket.send(bytes('log_shit','UTF-8'))
+        print(get_time() + " - Client at " + str(clientAddress) + " tried to log in with email : " + str(data[1]))
+    cursor.close()
+    db_connection.close()
+
 class ClientThread(threading.Thread):
-    def __init__(self,clientAddress,clientsocket):
+    def __init__(self,clientAddress, clientsocket):
         threading.Thread.__init__(self)
         self.csocket = clientsocket
         print(get_time() + " - New connection added: " + str(clientAddress))
     def run(self):
         msg = ['', '', '']
-        usr_list = ['Alexandre', 'admin']
-        pass_list = ['Test', 'admin']
         print(get_time() + " - Connection from : " + str(clientAddress))
-       
-        #data = self.csocket.recv(2048)
-        #msg = data.decode()
-        #print (datetime.datetime.now() , " - From " , clientAddress , " :", msg)
         data = self.csocket.recv(2048)
         msg[0] = data.decode()
         data = self.csocket.recv(2048)
@@ -24,16 +41,11 @@ class ClientThread(threading.Thread):
         data = self.csocket.recv(2048)
         msg[2] = data.decode()
         if msg[0] == 'log':
-            usr = msg[1]
-            psw = msg[2]
-            if usr in usr_list and psw in pass_list:
-                self.csocket.send(bytes('log_fine','UTF-8'))
-                print(get_time() + " - Client at " +  str(clientAddress) + " logged in with username : " + str(usr))
-            else:
-                self.csocket.send(bytes('log_shit','UTF-8'))
-                print(get_time() + " - Client at " + str(clientAddress) + " tried to log in with username : " + str(usr))
+            log_in(self, msg)
+
         print(get_time() + " - Client at " + str(clientAddress) + " disconnected...")
-        
+
+create_tables()
 LOCALHOST = "127.0.0.1"
 PORT = 8080
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
